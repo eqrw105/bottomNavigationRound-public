dc246145ec72eed3c2d18d732a497d0e8402461b
eqrw105 <eqrw105@github.com>
2021-05-17 22:40:42 +0900

    Initial commit

---
+++ app/src/main/java/com/nims/bottomnavigation/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.nims.bottomnavigation
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        navView.background = null
+        navView.menu.getItem(1).isEnabled = false
+    }
+}
